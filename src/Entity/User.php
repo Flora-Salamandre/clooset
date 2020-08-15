fb216fabc9e87bd52390be9dfedf0e8656a9b75d
Flora-Salamandre <f.strugala@yahoo.fr>
2020-08-15 19:31:06 +0200

    Entity User created.

---
+++ src/Entity/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="string", length=30)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $fisrtname;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getFisrtname(): ?string
+    {
+        return $this->fisrtname;
+    }
+
+    public function setFisrtname(string $fisrtname): self
+    {
+        $this->fisrtname = $fisrtname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
